c08fc2612834c828e267b0ce8de79f6c7f4b6868
David Rosales <137208896+plunkydev@users.noreply.github.com>
2023-09-18 17:09:45 -0400

    // Creada funcion playRound para comparar la desicion de player contra computer y asignar puntuacion al ganador

--- script.js
+++ script.js
@@ -11,7 +11,26 @@
 // crear variables para llevar la puntuacion del jugador y la cumputadora
 let player = 0;
 let computer = 0;
- console.log(getComputerChoice());
+// Crear funcion playRound para comparar la desicion de player contra computer y asignar puntuacion al ganador
+const playRound = (playerSelection, computerSelection) => {
+   playerSelection.toLowerCase();
+     if(playerSelection === "piedra" && computerSelection === "tijeras") {
+       player++;
+       return "You win";
+     } else if(playerSelection === "tijeras" && computerSelection === "papel") {
+       player++;
+         return "You win";
+     } else if(playerSelection === "papel" && computerSelection === "piedra") {
+       player++;
+       return "You win";
+     } else if(playerSelection === computerSelection) {
+       return "Equal"
+     } else {
+       computer++;
+       return "You loose"
+     }
+   }
+
  
  
  
